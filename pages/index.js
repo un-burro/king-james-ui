dadc5637b905112b565f2b4dbf7dd489e84585d8
Salim Ali <u14262208@tuks.co.za>
2023-05-27 11:32:09 +0200

    inserted nav bar component inside home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-// import styles from "@/styles/Home.module.css";
+import NavBar from "@/components/NavBar"; 
 
 
 export default function Home() {
@@ -13,6 +13,8 @@ export default function Home() {
       </Head>
 
       <main>
+
+        <NavBar />
 
       </main>
 
