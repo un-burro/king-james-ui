7dbf33e2dc01740e60ac950c9521b11f6be5c899
Salim Ali <u14262208@tuks.co.za>
2023-05-27 16:23:15 +0200

    mapped out services data to info tiles

--- components/ServiceOfferings/index.js
+++ components/ServiceOfferings/index.js
@@ -1,6 +1,7 @@
 import styles from "./styles.module.css";
 import SectionTag from "../SectionTag";
-import { constants } from "../../public/constants";
+import InfoTile from "../InfoTile";
+import { services } from "../../public/constants";
 
 
 export default function ServiceOfferings() {
@@ -16,7 +17,17 @@ export default function ServiceOfferings() {
                 <p className={styles.heading}>We offer a complete range of bespoke design and development services to help you turn your ideas into digital masterpieces</p>
             </div>
 
-            
+            <div className={styles.offeringsSection}>
+
+                { services.map((item, index) => <InfoTile image={item.image}
+                                                          key={index}
+                                                          title={item.title}
+                                                          description={item.description} />) }
+
+            </div>
+
+            <SectionTag title="Case studies" />
+
 
         </div>
 
