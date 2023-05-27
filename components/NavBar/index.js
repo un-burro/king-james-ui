a90b70bc496b614f028e4b6f7a8ff4aafb9fb25f
Salim Ali <u14262208@tuks.co.za>
2023-05-27 12:32:02 +0200

    added links and pointer cursors on button hover, adjusted item spacing

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -10,20 +10,39 @@ export default function NavBar() {
 
             <div className={styles.contentSection}>
 
-                <div className={styles.logoContainer}>
+                <a href="#">
 
                     <Image src="/assets/icons/logo_KJD.svg"
-                           width={53.29}
-                           height={32} />
+                            width={53.29}
+                            height={32} />
+
+                </a>
+
+                <div className={styles.menuContainer}>
+    
+                    <a href="#">
+                        <p className={styles.text}>Services</p>
+                    </a>
+
+                    <a href="#">
+                        <p className={styles.text}>Industries</p>
+                    </a>
+
+                    <a href="#">
+                        <p className={styles.text}>Cases</p>
+                    </a>
+
+                    <a href="#">
+                        <p className={styles.text}>Contact</p>
+                    </a>
 
                 </div>
 
-                <div className={styles.menuContainer}>
-
-                    <p className={styles.text}>Services</p>
-                    <p className={styles.text}>Industries</p>
-                    <p className={styles.text}>Cases</p>
-                    <p className={styles.text}>Contact</p>
+                <div className={styles.ctaContainer}>
+    
+                    <a href="#">
+                        <button className={styles.ctaButton}>Let's Talk</button>
+                    </a>
 
                 </div>
 
