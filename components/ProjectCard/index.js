db9ed0c9afc88802b76c6c1aa4cd8f62ff516418
Salim Ali <u14262208@tuks.co.za>
2023-05-27 16:41:11 +0200

    created project card component

--- components/ProjectCard/index.js
+++ components/ProjectCard/index.js
@@ -1,11 +1,37 @@
 import styles from "./styles.module.css";
 
 
-export default function ProjectCard() {
+export default function ProjectCard({ image, title, description }) {
+
+    const cardStyling = { backgroundImage: `linear-gradient(to bottom, 
+                                                            rgba(0, 0, 0, 0) 0%, 
+                                                            rgba(0, 0, 0, 0) 59%, 
+                                                            rgba(0, 0, 0, 0.65) 100%), 
+                                            
+                                            url(${image})` };
 
     return (
 
-        <div>
+        <div style={cardStyling}
+             className={styles.projectContainer}>
+
+            <div className={styles.overlay}>
+
+                <div className={styles.line} />
+    
+                <div className={styles.titleContainer}>
+
+                    <p className={styles.title}>{title}</p>
+
+                </div>
+
+                <div className={styles.descriptionContainer}>
+
+                    <p className={styles.text}>{description}</p>
+
+                </div>
+
+            </div>
 
         </div>
 
