b66a5e11f3d39950a4545fed6f8d605067411d82
Salim Ali <u14262208@tuks.co.za>
2023-05-27 11:44:07 +0200

    added kjd logo

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,11 +1,24 @@
 import styles from "./styles.module.css";
+import Image from "next/image";
 
 
 export default function NavBar() {
 
     return (
 
-        <div>
+        <div className={styles.navContainer}>
+
+            <div className={styles.contentSection}>
+
+                <div className={styles.logoContainer}>
+
+                    <Image src="/assets/icons/logo_KJD.svg"
+                           width={53.29}
+                           height={32} />
+
+                </div>
+
+            </div>
 
         </div>
 
